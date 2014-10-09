f135d6c32cf6e0f7f47b9b97dd8eba31b7a98921
s <s>
2014-10-09 09:02:19 +0800

    create file pair h/cpp for function resize

--- resizeC/main.cpp
+++ resizeC/main.cpp
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
+#include "resizeNN.h"
 
-void resize( char* pIn, char* pOut, int widthIn, int heightIn, int widthOut, int heightOut );
 void print( char* p, int width, int height );
 
 void main()
@@ -13,19 +13,6 @@ void main()
 	resize( &img_in[0][0], &img_out[0][0], 4, 4, 8, 8 );
 
 	print( &img_out[0][0], 8, 8 );
-}
-
-void resize( char* pIn, char* pOut, int widthIn, int heightIn, int widthOut, int heightOut )
-{
-	for (int i=0;i<heightOut;i++)
-	{
-		int iIn = i * heightIn / heightOut;
-		for (int j=0;j<widthOut;j++)
-		{
-			int jIn = j * widthIn / widthOut;
-			pOut[ i*widthOut + j ] = pIn[ iIn*widthIn + jIn ];
-		}
-	}
 }
 
 void print( char* p, int width, int height )
---
+++ resizeC/resizeNN.h
@@ -0,0 +1,2 @@
+
+void resize( char* pIn, char* pOut, int widthIn, int heightIn, int widthOut, int heightOut );
